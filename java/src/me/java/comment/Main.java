d5127dbefd45ef8e9c977393be436ed0f1ad0ce7
echoiing <qwertyuiop7823@gmail.com>
2022-09-20 00:20:01 +0900

    First Commit

---
+++ java/src/me/java/comment/Main.java
@@ -0,0 +1,32 @@
+package me.java.comment; // 패키지 (클래스 집합, 이름은 소문자로 작성)
+
+public class Main { // 클래스 (이름은 대문자로 작성, Upper Camel Case
+    public static void main(String[] args) { // 메인 함수 (프로그램 실행시 자동으로 호출되어 순차적으로 문장이 실행됨)
+        /*
+         * this is javadoc comment
+         * it will be included at .class file (after compile)
+         * */
+
+        // 주석 (프로그램 코드에 영향을 주지 않고 코드의 설명이나 협업을 위해 사용)
+        // ex) 코드 설명, 개발자 이름, 수정일
+
+        // 단일행 주석
+        /* 다중행 주석
+         * ㄴㅁㅇㅁㄴㅇ
+         * ㅁㄴㅇㅁㄴㅇ
+         * ㅁㄴㅇㅁㄴㅇ*/
+
+        // 문장 끝에는 세미콜론 (;) 작성
+        System.out.print("Hello Java World - 1"); // 콘솔에 출력하는 함수
+        System.out.print("Hello Java World - 2");
+        System.out.print("Hello Java World - 3");
+        System.out.print("Hello Java World - 4");
+        System.out.print("Hello Java World - 5");
+
+        // 콘솔에 출력하는 함수
+        System.out.println("1"); // 콘솔에 출력하는 함수 (문장 + 엔터)
+        System.out.print("2"); // 콘솔에 출력하는 함수 (문장만 출력)
+        System.out.printf("3"); // 콘솔에 출력하는 함수 (포맷에 맞춰 출력)
+        System.out.println();
+    }
+}
---
+++ java/src/me/java/printf/Main.java
@@ -0,0 +1,99 @@
+package me.java.printf;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 12.34f;
+        double d = 56.78;
+        char c = 'a';
+        String s = "Hello World";
+        boolean b = true;
+
+        // printf 함수를 쓰는 이유
+        // month: 1 ~ 12 (최대 2글자)
+        // day: 1 ~ 30 (최대 2글자)
+        int month = 1;
+        int day = 1;
+        System.out.printf("month = %2d, day = %2d\n", month, day);
+        month = 12;
+        day = 30;
+        System.out.printf("month = %2d, day = %2d\n", month, day);
+
+        // int
+        System.out.printf("%d\n", i);
+        System.out.printf("%10d\n", i); // 오른쪽으로 정렬
+        System.out.printf("%-10d\n", i); // 왼쪽으로 정렬
+        System.out.printf("%010d\n", i);
+        System.out.printf("%o\n", i);
+        System.out.printf("%x\n", i);
+        System.out.printf("%X\n", i);
+        System.out.printf("%n");
+        System.out.println();
+
+        // float
+        System.out.printf("%f\n", f);
+        System.out.printf("%.2f\n", f);
+        System.out.printf("%.8f\n", f); // 부동 소수점의 오차 (**)
+        System.out.printf("%5.2f\n", f);
+        System.out.printf("%-5.2f\n", f);
+        System.out.printf("%e\n", f);
+        System.out.printf("%E\n", f);
+        System.out.printf("%30.3e\n", f);
+        System.out.printf("%30.3E\n", f);
+        System.out.println();
+
+        // double
+        System.out.printf("%f\n", d);
+        System.out.printf("%.2f\n", d);
+        System.out.printf("%.8f\n", d);
+        System.out.printf("%5.2f\n", d);
+        System.out.printf("%-5.2f\n", d);
+        System.out.printf("%e\n", f);
+        System.out.printf("%E\n", f);
+        System.out.printf("%30.3e\n", f);
+        System.out.printf("%30.3E\n", f);
+        System.out.println();
+
+        // char
+        System.out.printf("%c\n", c);
+        System.out.printf("%C\n", c);
+        System.out.printf("%30c\n", c);
+        System.out.printf("%30C\n", c);
+        System.out.println();
+
+        // String
+        System.out.printf("%s\n", s);
+        System.out.printf("%S\n", s);
+        System.out.printf("%30s\n", s);
+        System.out.printf("%30S\n", s);
+        System.out.println();
+
+        // boolean
+        System.out.printf("%b\n", b);
+        System.out.printf("%B\n", b);
+        System.out.printf("%30b\n", b);
+        System.out.printf("%30B\n", b);
+        System.out.println();
+
+
+        // 변수에 저장하지 않은 리터럴도 가능
+        System.out.printf("%d\n", 12345);
+        System.out.printf("%f\n", 12.34);
+        System.out.printf("%c\n", 'a');
+        System.out.printf("%s\n", "a");
+        System.out.printf("%b\n", true);
+        System.out.println();
+
+
+        // 변수의 자료형과 맞지 않은 지시자 사용 - 런타임 오류 발생 (IllegalFormatConversionException)
+//        System.out.printf("%c", i);
+        System.out.printf("%d", c);
+//        System.out.printf("%d", b);
+
+        // 존재하지 않은 지시자 사용 - 런타임 오류 발생 (IllegalFormatConversionException)
+//        System.out.printf("%a", i);
+//        System.out.printf("%k", i);
+//        System.out.printf("%q", i);
+
+    }
+}
