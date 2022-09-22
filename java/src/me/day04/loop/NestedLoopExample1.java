6b57075bbb8f0359cd8c243159cfa98c7852c8b7
echoiing <qwertyuiop7823@gmail.com>
2022-09-23 02:06:14 +0900

    revised NestedLoopExample1.java

--- java/src/me/day04/loop/NestedLoopExample1.java
+++ java/src/me/day04/loop/NestedLoopExample1.java
@@ -2,10 +2,10 @@ package me.day04.loop;
 
 public class NestedLoopExample1 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 5; j++) {
+        for (int i = 1; i <= 5; i++) { // i 생성
+            for (int j = 1; j <= 5; j++) { // j 생성
                 System.out.println("i = " + i + ", j = " + j);
-            }
+            } // j 소멸
             System.out.println();
         }
     }
