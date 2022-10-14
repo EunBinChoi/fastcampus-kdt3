38f9dd7a47746919a889e277bd227d0c1f32b445
echoiing <qwertyuiop7823@gmail.com>
2022-10-15 06:31:02 +0900

    initial commit

---
+++ java/src/me/day14/practice/practice05/Main.java
@@ -0,0 +1,16 @@
+package me.day14.practice.practice05;
+
+public class Main {
+    public static void main(String[] args) {
+        String jsonString = "{\n" +
+                            "  \"name\": \"Madame Uppercut\",\n" +
+                            "  \"age\": 39,\n" +
+                            "  \"secretIdentity\": \"Jane Wilson\",\n" +
+                            "  \"powers\": [\n" +
+                            "    \"Million tonne punch\",\n" +
+                            "    \"Damage resistance\",\n" +
+                            "    \"Superhuman reflexes\"\n" +
+                            "  ]\n" +
+                            "}";
+    }
+}
