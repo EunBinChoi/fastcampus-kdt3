627e0d3409c73e77bd226902846fc7e9f5938f6d
echoiing <qwertyuiop7823@gmail.com>
2022-11-03 12:36:01 +0900

    initial commit

---
+++ spring/controller/src/main/java/org/example/overview/members/database/ConnectionPoolMgr.java
@@ -0,0 +1,284 @@
+package org.example.overview.members.database;
+
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+import java.util.Vector;
+
+/**
+ * Manages a java.sql.Connection pool.
+ *
+ * @author  Anil Hemrajani
+ */
+
+public class ConnectionPoolMgr {
+    private Vector connections = new Vector(10);
+
+
+    private String _driver = "org.h2.Driver",
+            _url = "jdbc:h2:~/JDBC",
+            _user = "sa",
+            _password = "";
+    private boolean _traceOn = false;
+    private boolean initialized = false;
+    private int _openConnections = 10;
+    private static ConnectionPoolMgr instance = null;
+
+    public ConnectionPoolMgr() {}
+
+    /** Use this method to set the maximum number of open connections before
+     unused connections are closed.
+     */
+
+    // 현재 놀고 있는 DB 객체 반환
+    public static ConnectionPoolMgr getInstance() {
+        if (instance == null) {
+            synchronized (ConnectionPoolMgr.class) {
+                if (instance == null) {
+                    instance = new ConnectionPoolMgr();
+                }
+            }
+        }
+        return instance;
+    }
+
+    // DB 커넥션 풀 객체 개수 set (10 -> count)
+    public void setOpenConnectionCount(int count) {
+        _openConnections = count;
+    }
+
+    public void setEnableTrace(boolean enable) {
+        _traceOn = enable;
+    }
+
+    // DB 커넥션 풀 객체 리스트 반환
+    /** Returns a Vector of java.sql.Connection objects */
+    public Vector getConnectionList() {
+        return connections;
+    }
+
+    /** Opens specified "count" of connections and adds them to the existing pool */
+    public synchronized void setInitOpenConnections(int count)
+            throws SQLException {
+        Connection c = null;
+        ConnectionObject co = null;
+
+        for (int i = 0; i < count; i++) {
+            c = createConnection();
+            co = new ConnectionObject(c, false);
+            connections.addElement(co);
+            trace("ConnectionPoolManager: Adding new DB connection to pool (" + connections.size() + ")");
+        }
+    }
+
+    /** Returns a count of open connections */
+    public int getConnectionCount() {
+        return connections.size();
+    }
+
+    // 커넥션 객체 반환
+    /** Returns an unused existing or new connection.  */
+    public synchronized Connection getConnection()
+            throws Exception {
+        if (!initialized) {
+            Class c = Class.forName(_driver);
+            DriverManager.registerDriver((Driver) c.newInstance());
+            initialized = true;
+        }
+        Connection c = null;
+        ConnectionObject co = null;
+        boolean badConnection = false;
+
+        for (int i = 0; i < connections.size(); i++) {
+            co = (ConnectionObject) connections.get(i);
+            // If connection is not in use, test to ensure it's still valid!
+            if (!co.inUse) {
+                try {
+                    badConnection = co.connection.isClosed();
+                    if (!badConnection)
+                        badConnection = (co.connection.getWarnings() != null);
+                } catch (Exception e) {
+                    badConnection = true;
+                    e.printStackTrace();
+                }
+                // Connection is bad, remove from pool
+                if (badConnection) {
+                    connections.removeElementAt(i);
+                    trace("ConnectionPoolManager: Remove disconnected DB connection #" + i);
+                    continue;
+                }
+                c = co.connection;
+                co.inUse = true;
+                trace("ConnectionPoolManager: Using existing DB connection #" + (i + 1));
+                break;
+            }
+        }
+
+        if (c == null) {
+            c = createConnection();
+            co = new ConnectionObject(c, true);
+            connections.addElement(co);
+            trace("ConnectionPoolManager: Creating new DB connection #" + connections.size());
+        }
+        return c;
+    }
+
+    // DB 커넥션 객체 반환 (close() 대신 사용)
+    /** Marks a flag in the ConnectionObject to indicate this connection is no longer in use */
+    public synchronized void freeConnection(Connection c) {
+        if (c == null)
+            return;
+
+        ConnectionObject co = null;
+
+        for (Object connection : connections) {
+            co = (ConnectionObject) connection;
+            if (c == co.connection) {
+                co.inUse = false;
+                break;
+            }
+        }
+
+        for (int i = 0; i < connections.size(); i++) {
+            co = (ConnectionObject) connections.get(i);
+            if ((i + 1) > _openConnections && !co.inUse)
+                removeConnection(co.connection);
+        }
+    }
+
+    public void freeConnection(Connection c, PreparedStatement p, ResultSet r) {
+        try {
+            if (r != null) r.close();
+            if (p != null) p.close();
+            freeConnection(c);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void freeConnection(Connection c, Statement s, ResultSet r) {
+        try {
+            if (r != null) r.close();
+            if (s != null) s.close();
+            freeConnection(c);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void freeConnection(Connection c, PreparedStatement p) {
+        try {
+            if (p != null) p.close();
+            freeConnection(c);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void freeConnection(Connection c, Statement s) {
+        try {
+            if (s != null) s.close();
+            freeConnection(c);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /** Marks a flag in the ConnectionObject to indicate this connection is no longer in use */
+    public synchronized void removeConnection(Connection c) {
+        if (c == null)
+            return;
+
+        ConnectionObject co = null;
+        for (int i = 0; i < connections.size(); i++) {
+            co = (ConnectionObject) connections.get(i);
+            if (c == co.connection) {
+                try {
+                    c.close();
+                    connections.removeElementAt(i);
+                    trace("Removed " + c.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
+    }
+
+    private Connection createConnection()
+            throws SQLException {
+        Connection con = null;
+
+        try {
+            if (_user == null)
+                _user = "";
+            if (_password == null)
+                _password = "";
+
+            Properties props = new Properties();
+            props.put("user", _user);
+            props.put("password", _password);
+
+            con = DriverManager.getConnection(_url, props);
+        } catch (Throwable t) {
+            throw new SQLException(t.getMessage());
+        }
+        return con;
+    }
+
+    /** Closes all connections and clears out the connection pool */
+    public void releaseFreeConnections() {
+        trace("ConnectionPoolManager.releaseFreeConnections()");
+
+        Connection c = null;
+        ConnectionObject co = null;
+
+        for (Object connection : connections) {
+            co = (ConnectionObject) connection;
+            if (!co.inUse)
+                removeConnection(co.connection);
+        }
+    }
+
+    /** Closes all connections and clears out the connection pool */
+    @Override
+    public void finalize() {
+        trace("ConnectionPoolManager.finalize()");
+
+        Connection c = null;
+        ConnectionObject co = null;
+
+        for (Object connection : connections) {
+            co = (ConnectionObject) connection;
+            try {
+                co.connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            co = null;
+        }
+        connections.removeAllElements();
+    }
+
+    private void trace(String s) {
+        if (_traceOn)
+            System.err.println(s);
+    }
+}
+
+class ConnectionObject {
+    public java.sql.Connection connection = null;
+    public boolean inUse = false;
+
+    public ConnectionObject(Connection c, boolean useFlag) {
+        connection = c;
+        inUse = useFlag;
+    }
+}
---
+++ spring/controller/src/test/java/org/example/overview/members/controller/SignupControllerTest.java
@@ -0,0 +1,52 @@
+package org.example.overview.members.controller;
+
+import org.example.overview.config.DispatcherServletConfig;
+import org.example.overview.config.WebAppConfig;
+import org.example.overview.members.controller.nonlogin.SignupController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class) // SpringJunit4ClassRunner.class는 spring-test에서 제공하는 단위테스르를 위한 클래스 러너
+@WebAppConfiguration // WebApplicationContext 생성할 수 있도록 하는 어노테이션
+@ContextConfiguration(classes = {WebAppConfig.class, DispatcherServletConfig.class})
+public class SignupControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext; // MockMvc 객체 생성을 위한 context
+    private MockMvc mockMvc; // controller에 request를 수행해주는 mock 객체
+
+    @Autowired
+    private SignupController signupController; // 테스트를 진행할 컨트롤러
+
+
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(signupController).build();     // test를 위한 MockMvc 객체 생성. signupController 1개만 주입.
+//        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();  // test를 위한 MockMvc 객체 생성. 스프링이 로드한 WebApplicationContext의 인스턴스로 작동.(standaloneSetup() 중 택 1)
+    }
+
+    @Test
+    public void doSignupTest() throws Exception {
+        mockMvc.perform(post("/members/signup")
+                        .param("uEmail", "asd@gmail.com")
+                        .param("uId", "")
+                        .param("uPw", "a1234")
+                        .session(new MockHttpSession()))
+                        .andExpect(status().isOk())
+                        .andDo(print());
+    }
+}
