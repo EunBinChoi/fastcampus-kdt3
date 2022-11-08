0055214f62f836cc98e8ca1a6a0c9dad02e751ad
echoiing <qwertyuiop7823@gmail.com>
2022-11-08 10:26:01 +0900

    style: add comment

--- spring/logging/src/main/java/org/example/overview/log/MainClass.java
+++ spring/logging/src/main/java/org/example/overview/log/MainClass.java
@@ -4,8 +4,8 @@ import org.example.overview.log.job.CustomCronTrigger;
 
 public class MainClass {
     public static void main(String[] args) {
-        // LogExample logExample = new LogExample();
-        // logExample.printLog();
+//        LogExample logExample = new LogExample();
+//        logExample.printLog();
 
         CustomCronTrigger customCronTrigger = new CustomCronTrigger();
         customCronTrigger.start();
